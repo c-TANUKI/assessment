ed9196fbbba85e3774ca86b3e025072e6c05c9d9
c-TANUKI <shinnosukefujii0301@gmail.com>
2025-11-26 21:34:02 +0900

    Bootstrapを使用してアレンジしたもの

--- assessment.js
+++ assessment.js
@@ -27,7 +27,7 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText = '';
-    const header = document.createElement('h3');
+    /*const header = document.createElement('h3');
     header.innerText = '診断結果';
     resultDivision.appendChild(header);
     /*createElementは要素を作成するメソッド
@@ -35,10 +35,31 @@ assessmentButton.addEventListener(
     で指定したタグの要素を作成する。
     */ 
 
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
+
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
+    /*const paragraph = document.createElement('p');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    resultDivision.appendChild(paragraph);*/
     /*appendChildは、指定した要素の子要素として追加するメソッド
     親要素.appendChild(子要素);
     で指定した親要素の子要素として、指定した子要素を追加する。
